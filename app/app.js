59ee8b838097f45966d3ab896683c516ea33f512
fudexuan <457334561@qq.com>
2016-11-09 17:19:31 +0800

    模板

--- app/app.js
+++ app/app.js
@@ -1,12 +1,18 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
+	angular.module('myApp', [
+	  'ui.router'
+	])
+	.config(['$stateProvider','$UrlRouterProvider', function($stateProvider,$urlRouterProvider) {
+		$stateProvider
+			.state('view1',{
+				url:'/view1',
+				templateUrl:'./view1/view1.html'
+			})
+			.state('view2',{
+				url:'/view2',
+				templateUrl:'./view2/view2.html'
+			})
+			$urlRouterProvider.otherwise("/view1");
 }]);
